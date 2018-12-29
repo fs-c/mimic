e7dfc0fdb938036bd95cd574203a69f37b6007cb
lw <me@lw.ag>
2018-12-29 19:24:04 +0100

    Implement replay file parsing

--- src/mimic.h
+++ src/mimic.h
@@ -8,6 +8,25 @@
 
 typedef unsigned char BYTE;
 
+struct replay_meta {
+	BYTE mode;
+	int32_t version;
+	char *beatmap_hash;
+	char *player_name;
+	char *replay_hash;
+	int32_t score;
+	int32_t max_combo;
+	BYTE perfect;
+	int32_t mods_used;
+	char *life_bar;
+	int64_t timestamp;
+};
+
+struct replay_data {
+	size_t raw_len;
+	BYTE *raw;
+};
+
 void set_stream(FILE *new_stream);
 
 BYTE read_byte();
@@ -19,5 +38,8 @@ size_t read_osu_string(char *out_buffer);
 
 int decompress_basic(BYTE *in_data, size_t in_len, BYTE **out_data,
 	size_t *out_len);
+
+int parse_replay(FILE *file, struct replay_meta *meta,
+	struct replay_data *data);
 
 #endif /* MIMIC_H */
---
+++ src/replay.c
@@ -0,0 +1,83 @@
+#include "mimic.h"
+
+static void assign_string(char **dest);
+
+char string_buffer[2048];
+
+int parse_replay(FILE *file, struct replay_meta *meta, struct replay_data *data)
+{
+	if (!file || !meta) {
+		return 1;
+	}
+
+	set_stream(file);
+
+	meta->mode = read_byte();
+	meta->version = read_int32();
+
+	assign_string(&meta->beatmap_hash);
+	assign_string(&meta->player_name);
+	assign_string(&meta->replay_hash);
+
+	/* Discard the next 6 shorts */
+	fread(string_buffer, sizeof(int16_t), 6, file);
+
+	meta->score = read_int32();
+	meta->max_combo = read_int16();
+	meta->perfect = read_byte();
+	meta->mods_used = read_int32();
+	
+	assign_string(&meta->life_bar);
+
+	meta->timestamp = read_int64();
+
+	/* Replay data parsing is optional */
+	if (!data) {
+		return 0;
+	}
+
+	int32_t comp_len = read_int32();
+	BYTE *comp = malloc(comp_len);
+
+	if (!comp) {
+		return 2;
+	}
+
+	for (int32_t i = 0, c; i < comp_len; i++) {
+		if ((c = fgetc(file)) == EOF) {
+			printf("reached EOF early, bailing out (%d)\n", i);
+
+			break;
+		}
+
+		comp[i] = c;
+	}
+
+	BYTE *decomp = NULL;
+	size_t decomp_len = 0;
+
+	if (decompress_basic(comp, comp_len, &data->raw, &data->raw_len)) {
+		printf("decompression failed\n");
+
+		return 1;
+	}
+
+	return 0;
+}
+
+static void assign_string(char **dest)
+{
+	size_t string_len = read_osu_string(string_buffer);
+
+	if (string_len <= 0) {
+		*dest = NULL;
+
+		return;
+	}
+
+	if (!(*dest = malloc(string_len))) {
+		return;
+	}
+
+	strncpy(*dest, string_buffer, string_len);
+}
